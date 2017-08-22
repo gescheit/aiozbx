544688b7ff60520a74b10d9a681fd888843af312
Aleksandr Balezin <gescheit@yandex-team.ru>
2017-08-22 14:50:58 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="aiozbx",
+      version="0.1",
+      description="aiozbx",
+      packages=["aiozbx"],
+      author="gescheit",
+      author_email="gescheit12@gmail.com",
+      url="https://github.com/gescheit/aiozbx",
+      )

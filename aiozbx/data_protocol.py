f44a91f4e378fcab6ce9f7b39d2bbbf814c9d9dd
Aleksandr Balezin <gescheit@yandex-team.ru>
2019-05-28 17:50:03 +0300

    fix SenderData

--- aiozbx/data_protocol.py
+++ aiozbx/data_protocol.py
@@ -19,22 +19,22 @@ class SenderData:
     pack_fmt = struct.Struct("<q")
     header = ZBX_TCP_HEADER_DATA + ZBX_TCP_HEADER_VERSION
 
-    @staticmethod
-    def pack_msg(cls, msg: bytes):
+    @classmethod
+    def pack_msg(cls, msg):
         res = bytearray()
         res += cls.header
         res += cls.pack_fmt.pack(len(msg))
         res += msg
         return bytes(res)
 
-    @staticmethod
-    def encode_msg(data):
+    @classmethod
+    def encode_msg(cls, data):
         req = {"request": ZBX_PROTO_VALUE_SENDER_DATA,
                "data": data,
                "clock": int(time.time()),
                "ns": 0}
         req = json.dumps(req, ensure_ascii=False).encode()
-        msg = SenderData.encode_msg(req)
+        msg = cls.pack_msg(req)
         return msg
 
     @staticmethod
